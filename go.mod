6d045a3ec93256796b2e4d0101750cab0eb326c6
cnuttens <clement.nutt@gmail.com>
2024-10-02 13:09:15 +0200

    Sarting to add some elements, header, slider, pop up, post form

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module login
 go 1.23
 
 require (
-	github.com/mattn/go-sqlite3 v1.14.23 // indirect
-	golang.org/x/crypto v0.27.0 // indirect
-	modernc.org/sqlite v1.33.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.23 // direct
+	golang.org/x/crypto v0.27.0 // direct
+	modernc.org/sqlite v1.33.1 // direct
 )
---
+++ web/static/css/dashboard.js
@@ -0,0 +1,47 @@
+const avatar = document.getElementById('avatar');
+const popup = document.getElementById('popup');
+const new_post = document.getElementById('new_post');
+const bttn_new_post = document.getElementById('create_post');
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Afficher ou cacher le pop-up lorsque l'avatar est cliqué
+    avatar.onclick = function(event) {
+        popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+        event.stopPropagation(); // Empêcher la propagation de l'événement
+    };
+
+    // Cacher le pop-up lorsque l'on clique en dehors
+    document.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+    // Afficher ou cacher le formulaire de création de post
+    bttn_new_post.onclick = function(event) {
+        new_post.style.display = new_post.style.display === 'block' ? 'none' : 'block';
+        event.stopPropagation(); // Empêcher la propagation de l'événement
+    };
+
+    // Cacher le formulaire lorsque l'on clique en dehors
+    document.addEventListener('click', function(event) {
+        // Vérifier si le clic a été fait à l'extérieur du formulaire
+        if (!new_post.contains(event.target) && !bttn_new_post.contains(event.target)) {
+            new_post.style.display = 'none';
+        }
+    });
+
+    // Cacher le pop-up lorsque l'on clique à l'intérieur du formulaire
+    new_post.onclick = function(event) {
+        event.stopPropagation(); // Empêcher la propagation de l'événement
+    };
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const settingsPanel = document.getElementById("settings_panel");
+    const toggleButton = document.getElementById("toggle_settings");
+    
+    toggleButton.addEventListener("click", function() {
+        settingsPanel.classList.toggle("collapsed");
+        toggleButton.textContent = settingsPanel.classList.contains("collapsed") ? "Afficher" : "Réduire";
+    });
+});
