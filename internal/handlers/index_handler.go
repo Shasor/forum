c07f1a820db569fb375ef38d5e081dfbc146a207
nsannier <Naofumi76@users.noreply.github.com>
2024-11-06 14:07:33 +0100

    Now can access site without login

--- internal/handlers/index_handler.go
+++ internal/handlers/index_handler.go
@@ -11,16 +11,18 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		Resp.Broadcasted = true
 	}
-
+	var likedposts []db.Post
 	// Get the user from the cookie, which may be nil
 	user := GetUserFromCookie(w, r)
-
+	if user != nil {
+		likedposts = db.FetchPostsLiked(user.ID)
+	}
 	categoryID, postID := GetFormGet(r)
 	data := map[string]interface{}{
 		"resp":       Resp,
 		"user":       user, // This will be nil if no user is logged in
 		"posts":      db.FetchPosts(categoryID, postID),
-		"likedposts": db.FetchPostsLiked(user.ID),
+		"likedposts": likedposts,
 	}
 
 	Parse(w, data)
