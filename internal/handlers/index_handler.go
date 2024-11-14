4a76e00e3754d989d19f9a0705a4f6005213a526
cnuttens <clement.nutt@gmail.com>
2024-11-14 13:01:14 +0100

    Get the search value in index_handler

--- internal/handlers/index_handler.go
+++ internal/handlers/index_handler.go
@@ -1,11 +1,27 @@
 package handlers
 
 import (
+	"fmt"
 	"forum/internal/db"
 	"net/http"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Erreur dans la requête", http.StatusBadRequest)
+			return
+		}
+
+		// Récupère la valeur de l'input `search_bar`
+		searchValue := r.FormValue("search_bar")
+
+		//--TO DO-- : Recharger la page avec la catégorie si cela correspond, ou par l'ensemble des catégories contenant la recherche "ima" affiche les catégories "image", "Fatima", ...
+		fmt.Println("Vous avez recherché : ", searchValue)
+	}
+
 	if Resp.Broadcasted {
 		Resp = Response{}
 	} else {
@@ -26,9 +42,5 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		"GET":        get,
 	}
 	Parse(w, data)
-
-	if r.Method == "POST" {
-		print(r.FormValue("SearchForm"))
-	}
 
 }
